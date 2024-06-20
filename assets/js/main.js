2545ef2ce879defa06cde42862b3ded09d68bb94
Gilbert S Cachaya Caicedo <47873666+GilbertCC@users.noreply.github.com>
2024-06-20 15:40:55 -0500

    pruebas finales

--- assets/js/main.js
+++ assets/js/main.js
@@ -259,7 +259,7 @@
    */
   window.addEventListener('load', () => {
     AOS.init({
-      duration: 1000,
+      duration: 500,
       easing: 'ease-in-out',
       once: true,
       mirror: false
@@ -272,3 +272,25 @@
   new PureCounter();
 
 })()
+
+/*--- formulario ---*/
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action,{
+      method: this.method,
+      body: form,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    if (response.ok){
+      this.reset()
+      alert('Gracias su mensaje fue enviado , le contactaremos pronto')
+    }
+}
